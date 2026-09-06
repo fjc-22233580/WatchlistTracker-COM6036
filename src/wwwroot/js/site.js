a7ade0ac1ae14a82ec73748b40244c6ddb0e4861
fjc-22233580 <22233580@bucks.ac.uk>
2026-09-06 16:09:59 +0100

    Made TempData fade out.

--- src/wwwroot/js/site.js
+++ src/wwwroot/js/site.js
@@ -1,4 +1,13 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+// Allow TempData messages to fade out.
+document.querySelectorAll(".auto-dismiss-alert").forEach(alert => {
+    setTimeout(() => {
+        alert.classList.remove("show");
+
+        setTimeout(() => {
+            alert.remove();
+        }, 500);
+    }, 3000);
+});
